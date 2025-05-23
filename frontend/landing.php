3f97bcbded5f70da74b2735049a8070d5d29b33d
Ronz0814 <ronzremoroza@gmail.com>
2025-05-23 20:02:56 +0100

    .

---
+++ frontend/landing.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&" />
+    <link rel="stylesheet" href="landing.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;
+    0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <aside class="sidebar">
+        <!-- Sidebar Header -->
+        <div class="sidebar-header">
+            <a href="#" class = "header-logo">
+                <h1 class="sidebar-title">ARM</h1>
+            </a>
+            <button class="toggler">
+                <span class="material-symbols-outlined">chevron_left</span>
+            </button> 
+        </div>
+
+        <!-- Sidebar Primaty nav -->
+         <nav class="sidebar-nav">
+            <ul class="nav-list primary-nav">
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <span class="nav-icon material-symbols-outlined">groups_3</span>
+                        <span class="nav-label">Teams</span>    
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <span class="nav-icon material-symbols-outlined">person</span>
+                        <span class="nav-label">Roster</span>    
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <span class="nav-icon material-symbols-outlined">history_2</span>
+                        <span class="nav-label">History</span>    
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <span class="nav-icon material-symbols-outlined">leaderboard</span>
+                        <span class="nav-label">Standing</span>    
+                    </a>
+                </li>
+                 <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <span class="nav-icon material-symbols-outlined">trophy</span>
+                        <span class="nav-label">Coach</span>    
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                        <a href="" class="nav-link">
+                            <span class="nav-icon material-symbols-outlined">call</span>
+                            <span class="nav-label">Contact Us</span>    
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link">
+                            <span class="nav-icon material-symbols-outlined">admin_panel_settings</span>
+                            <span class="nav-label">Admin</span>    
+                        </a>
+                    </li>
+            </ul>
+         </nav>
+    </aside>
+
+   
+</body>
+</html>
